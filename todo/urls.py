fe66be625cd3a9a31ecc3d50004bc433f225fe03
uly <ulyana.akinfina22@gmail.com>
2024-09-24 12:16:13 +0300

    Removing standard Authentication

--- todo/urls.py
+++ todo/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/v1/drf-auth/', include('rest_framework.urls')),  # # Стандартные эндпоинты аутентификации
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
 
